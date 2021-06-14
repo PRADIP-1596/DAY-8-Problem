0965750620a13a9161a0d83d490b530672c2df81
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-06-15 01:18:54 +0530

    [Pradip] add.first commit

---
+++ EmployeeWage.java
@@ -0,0 +1,11 @@
+public class EmployeeWageOops {
+	public static void main(String[] args) {
+   	int isPresent = 1;
+      double empCheck = Math.floor(Math.random() * 10) % 2;
+      System.out.println(empCheck);
+      if (empCheck == isPresent)
+      	System.out.println("present");
+      else
+      	System.out.println("absent");
+	}
+}
